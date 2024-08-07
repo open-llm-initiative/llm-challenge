1f947998b62654b4fe6738553b83701c0d1268e0
Salman Paracha <salmanparacha@MacBook-Pro-261.local>
2024-08-07 16:27:11 -0700

    updated project structure, committing for Co to add his changes

--- challenge.py
+++ challenge.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('render.html')  
+    return render_template('challenge.html')  
 
 def get_sample(sample_id):
   dynamodb = boto3.client('dynamodb')
