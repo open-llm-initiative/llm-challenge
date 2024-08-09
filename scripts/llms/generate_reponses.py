1d996f7459992101092c1051a91187525264a474
Salman Paracha <salmanparacha@MacBook-Pro-261.local>
2024-08-09 16:50:06 -0700

    making fixes and pushing to develop on the g6 instance

--- scripts/llms/generate_reponses.py
+++ scripts/llms/generate_reponses.py
@@ -5,6 +5,7 @@ import torch
 import pandas as pd
 import platform
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import ollama
 from openai import OpenAI
 
 
@@ -35,14 +36,15 @@ openai_models = {
 # Initialize a HuggingFace pipeline
 def initialize_hugging_face_llm_pipeline(model_name, model_id):
     model_pipeline = None
+    hf_token = os.getenv("HUGGINGFACE_TOKEN")
     try:
         if model_name == "Phi-3-small-128k-instruct":
             #load with trust_remote_code=True
-            tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+            tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True, )
             model = AutoModelForCausalLM.from_pretrained(modeil_id, trust_remote_code=True)
         else: 
-            tokenizer = AutoTokenizer.from_pretrained(model_id)
-            model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id, token=hf_token)
+            model = AutoModelForCausalLM.from_pretrained(model_id, token=hf_token)
 
         device = "cpu"
         if torch.cuda.is_available():
@@ -66,7 +68,6 @@ def get_hugging_face_llm_text_completion(pipeline, prompt, context):
     response = pipeline(prompt_to_pipeline, max_new_tokens=340, temperature=0.5, top_p=0.5, num_return_sequences=1, do_sample=True)
     return response[0]['generated_text'].split("#delimit#")[1]
 
-
 #free up local memory
 def release_huggingface_pipeline(pipeline):
     del pipeline
@@ -78,12 +79,13 @@ if __name__ == '__main__':
     print("Starting repsponse generation. First step: load LLM responses from OSS LLMs on HuggingFace")
     print("")
     challenge_prompt_df = pd.read_csv('../../data/challenge_setup.csv')
-    qwen_pipeline = initialize_hugging_face_llm_pipeline("Qwen2-1.5B","Qwen/Qwen2-1.5B")
+    qwen_pipeline = initialize_hugging_face_llm_pipeline("Qwen2-1.5B","google/gemma-2-2b")
 
     for row in challenge_prompt_df.itertuples():
         if row.Index == 2:
             print("Prompt:", row.prompt)
             print("Context:", row.context, "\n")
             print("Response:", get_hugging_face_llm_text_completion(qwen_pipeline, row.prompt, row.context))
+
 
     release_huggingface_pipeline(qwen_pipeline)
